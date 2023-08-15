52ddf2b7e473b52617b0208454aef954679fae6d
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-15 12:18:45 +0530

    Added products.tsx page.

--- apps/ai-market/src/app/page.tsx
+++ apps/ai-market/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import HeroSection from "@/components/hero-lan";
+import Products from "@/components/products-lan";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <Products />
     </div>
   );
 }
