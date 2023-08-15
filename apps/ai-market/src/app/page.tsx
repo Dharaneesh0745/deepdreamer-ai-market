8feff398e2ee3df9ad12c17a9d8e06a2f7ab9fb7
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-15 12:53:06 +0530

    Added newsletter.tsx page.

--- apps/ai-market/src/app/page.tsx
+++ apps/ai-market/src/app/page.tsx
@@ -6,6 +6,7 @@ import Subscriptions from "@/components/subscriptions-lan";
 import Testimonials from "@/components/testimonials-lan";
 import About from "@/components/about-lan";
 import Faq from "@/components/faq-lan";
+import Newsletter from "@/components/newsletter-lan";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Subscriptions />
       <Testimonials />
       <Faq />
+      <Newsletter />
     </div>
   );
 }
