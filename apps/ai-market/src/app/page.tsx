58d0865fbcd712a39b93fae7b7d0af58eccc7ea0
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-15 12:23:03 +0530

    Added subscriptions.tsx page.

--- apps/ai-market/src/app/page.tsx
+++ apps/ai-market/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 import HeroSection from "@/components/hero-lan";
 import Products from "@/components/products-lan";
+import Subscriptions from "@/components/subscriptions-lan";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <Products />
+      <Subscriptions />
     </div>
   );
 }
