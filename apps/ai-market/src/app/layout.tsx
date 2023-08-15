648dcd7c9cbeecd9df631486cac47863806bf19f
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-15 11:46:01 +0530

    Added footer.tsx in components.

--- apps/ai-market/src/app/layout.tsx
+++ apps/ai-market/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import Header from "@/components/header-lan";
+import Footer from "@/components/footer-lan";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
