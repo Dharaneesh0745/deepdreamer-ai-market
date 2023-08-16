a6f2a172186b5feeb3e7f7b65b8b5c762214993b
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-16 20:16:40 +0530

    Added profile image in about page.

--- apps/ai-market/src/components/about-lan.tsx
+++ apps/ai-market/src/components/about-lan.tsx
@@ -18,24 +18,19 @@ const About = () => {
           <div className="flex flex-col sm:flex-row mt-10">
             <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
               <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-10 h-10"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </svg>
+                <Image
+                  height={70}
+                  width={85}
+                  className="object-cover rounded-full"
+                  src="/me.jpg"
+                  alt="dev"
+                />
               </div>
               <div className="flex flex-col items-center text-center justify-center">
-                <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
-                  Phoebe Caulfield
+                <h2 className="font-medium title-font mt-4 text-orange-500 text-lg">
+                  Dharaneesh Boobalan
                 </h2>
-                <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                <div className="w-12 h-1 bg-neon-blue rounded mt-2 mb-4"></div>
                 <p className="text-base">
                   Raclette knausgaard hella meggs normcore williamsburg enamel
                   pin sartorial venmo tbh hot chicken gentrify portland.
