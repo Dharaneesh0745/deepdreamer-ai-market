0fe522fcb6d452ecc8c0f5526f020a9ff4e3be91
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-16 11:16:49 +0530

    Updated site dark color & updated hearder, hero.

--- apps/ai-market/src/components/header-lan.tsx
+++ apps/ai-market/src/components/header-lan.tsx
@@ -11,9 +11,13 @@ const Header = () => {
   };
 
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
-        <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <header
+      className={`top-0 left-0 right-0 ${
+        isNavOpen ? "bg-gray-900" : "bg-transparent"
+      } text-white dark:text-white transition-all duration-300 ease-in-out`}
+    >
+      <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+        <div className="ml-5 flex title-font font-medium items-center text-gray-900 md:mb-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -26,39 +30,57 @@ const Header = () => {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
-        </div>
-        <button
-          className="md:hidden text-gray-600 hover:text-gray-900"
-          onClick={toggleNav}
-        >
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
+          <span className="ml-3 text-xl text-orange-500">Deep Dreamer</span>
+          <button
+            className="md:hidden text-white hover:text-neon-blue ml-32 transition-all duration-300 ease-in-out"
+            onClick={toggleNav}
           >
-            {isNavOpen ? (
-              <path d="M6 18L18 6M6 6l12 12"></path>
-            ) : (
-              <path d="M4 6h16M4 12h16M4 18h16"></path>
-            )}
-          </svg>
-        </button>
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              {isNavOpen ? (
+                <path d="M6 18L18 6M6 6l12 12"></path>
+              ) : (
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+              )}
+            </svg>
+          </button>
+        </div>
+
         <nav
           className={`${
             isNavOpen ? "block" : "hidden"
-          } md:block md:ml-auto md:flex flex-wrap items-center text-base justify-center mt-4 md:mt-0`}
+          } md:ml-auto md:mr-auto md:flex flex-wrap items-center text-base justify-center mt-4 md:mt-0`}
         >
-          <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          <a
+            href="./about-lan.tsx"
+            className="block mt-4 md:inline-block md:mt-0 md:mr-4 hover:text-orange-500 transition-all duration-300 ease-in-out"
+          >
+            Developer
+          </a>
+          <a className="block mt-4 md:inline-block md:mt-0 md:mr-4 hover:text-orange-500 transition-all duration-300 ease-in-out">
+            Products
+          </a>
+          <a className="block mt-4 md:inline-block md:mt-0 md:mr-4 hover:text-orange-500 transition-all duration-300 ease-in-out">
+            Pricing
+          </a>
+          <a className="block mt-4 md:inline-block md:mt-0 md:mr-4 hover:text-orange-500 transition-all duration-300 ease-in-out">
+            Testimonials
+          </a>
+          <a className="block mt-4 md:inline-block md:mt-0 md:mr-4 hover:text-orange-500 transition-all duration-300 ease-in-out">
+            FAQ`s
+          </a>
+          <a className="block mt-4 md:inline-block md:mt-0 hover:text-orange-500 transition-all duration-300 ease-in-out">
+            Contact
+          </a>
         </nav>
-        <button className="hidden md:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        <button className="mr-3 hidden md:inline-flex items-center bg-orange-500 border-0 py-1 px-3 focus:outline-none hover:bg-neon-blue hover:text-black rounded text-base mt-4 md:mt-0">
           Button
           <svg
             fill="none"
@@ -66,7 +88,7 @@ const Header = () => {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
-            className="w-4 h-4 ml-1"
+            className="w-4 h-4 ml-1 -mt-1"
             viewBox="0 0 24 24"
           >
             <path d="M5 12h14M12 5l7 7-7 7"></path>
