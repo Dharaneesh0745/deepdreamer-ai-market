88a54840a1e92a5e3e784a141b89eb003c74d15e
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-20 09:17:57 +0530

    Created code, conversation, image, music, video, playground pages with constants.ts file for passing prompt to zod and formSchema for assigning objects & modified home page.

---
+++ apps/ai-market/app/(home)/(routes)/music/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { FileAudio, MessageSquare } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+
+import { Button } from "../../../../components/ui/button";
+import { Input } from "../../../../components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "../../../../components/ui/form";
+import { cn } from "../../../../lib/utils";
+
+import { formSchema } from "./constants";
+import { Heading } from "../../../../components/heading";
+import { Empty } from "../../../../components/empty";
+import { Loader } from "../../../../components/loader";
+import { UserAvatar } from "../../../../components/user-avatar";
+import { BotAvatar } from "../../../../components/bot-avatar";
+
+const MusicPage = () => {
+  const router = useRouter();
+  const [music, setMusic] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
+
+      const response = await axios.post("/api/music", values);
+
+      setMusic(response.data.audio);
+      form.reset();
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Our most advanced conversation model."
+        icon={FileAudio}
+        iconcolor="text-violet-500"
+        bgcolor="bg-violet-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Ask Deep Dreamer to generate music for you :)"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Request
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && (
+            <Empty label={"No music generated/not found in the database."} />
+          )}
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MusicPage;
---
+++ apps/ai-market/app/(home)/(routes)/playground/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaygroundPage = () => {
+  return <div>Comming Soon</div>;
+};
+
+export default PlaygroundPage;
