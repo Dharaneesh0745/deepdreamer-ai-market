0d39da7e05c8434d3b4e4f17e1c4238eb305c2ef
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-08-23 22:59:24 +0530

    Added schema.prisma file for db connect.

--- apps/ai-market/app/layout.tsx
+++ apps/ai-market/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Deep Dreamer | AI Market",
   description:
-    "Deep Dreamer (AI-market), this platform contains AI & ML products, where user can rent our products and Deep Dreamer have a playground facility so user can able to build their own AI modal. Users can able to use our APIs as open source (APIs are completely developed by Deep Dreamer from scratch). Visitors can also able to buy source codes from Deep Dreamer store/market, our store contains the latest cutting-edge technologies (source code). Some free source codes also available in our store. And also Deep Dreamer has Documentation page, this documentation page will guide the users to how to develop ML modals and also guide to use our source codes. (Deep Dreamer is under development)",
+    "Deep Dreamer (AI-market), this platform contains AI & ML products, where user can rent our products and Deep Dreamer have a playground facility so user can able to build their own AI modal. Users can able to use our APIs as open source (APIs are completely developed by Deep Dreamer from scratch). Visitors can also able to buy source codes from Deep Dreamer store/market, our store contains the latest cutting-edge technologies (source code). Some free source codes also available in our store. And also Deep Dreamer has Documentation page, this documentation page will guide the users to how to develop ML modals and also guide to use our source codes.",
 };
 
 export default function RootLayout({
